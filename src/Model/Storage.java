6550fcf4ac806005a12f1c732ffe34bcc3f4903e
Derek <7885194+derekstephen@users.noreply.github.com>
2019-02-25 17:35:03 -0500

    Added TODO Comment
    Just for a reminder on what to do

--- src/Model/Storage.java
+++ src/Model/Storage.java
@@ -15,6 +15,8 @@ import javax.swing.JOptionPane;
  * @author dsd5227
  */
 public class Storage {
+    
+    //TODO: Sort Lists after Adding new Items!!!
 
     ArrayList<Student> alStudent;
     ArrayList<Room> alRoom;
