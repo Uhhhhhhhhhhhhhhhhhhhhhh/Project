0195fc3c2e9ad088e57f4b79fb5fcd9a26b71d80
Derek <7885194+derekstephen@users.noreply.github.com>
2019-02-26 02:31:59 -0500

    Update Storage.java
    Encapsulation

--- src/Model/Storage.java
+++ src/Model/Storage.java
@@ -19,25 +19,15 @@ public class Storage {
     
     //TODO: Sort Lists after Adding new Items!!!
 
-    ArrayList<Student> alStudent;
-    ArrayList<Room> alRoom;
-    ArrayList<Faculty> alFaculty;
-    ArrayList<Section> alSection;
-    ArrayList<Course> alCourse;
-    ArrayList<Time_Period> alTime;
-    ArrayList<Final_Course_Assignment> alFCA;
+    private static ArrayList<Student> alStudent = new ArrayList<>();
+    private static ArrayList<Room> alRoom = new ArrayList<>();
+    private static ArrayList<Faculty> alFaculty = new ArrayList<>();
+    private static ArrayList<Section> alSection = new ArrayList<>();
+    private static ArrayList<Course> alCourse = new ArrayList<>();
+    private static ArrayList<Time_Period> alTime = new ArrayList<>();
+    private static ArrayList<Final_Course_Assignment> alFCA = new ArrayList<>();
 
-    public Storage() {
-        alStudent = new ArrayList<>();
-        alRoom = new ArrayList<>();
-        alFaculty = new ArrayList<>();
-        alSection = new ArrayList<>();
-        alCourse = new ArrayList<>();
-        alTime = new ArrayList<>();
-        alFCA = new ArrayList<>();
-    }
-
-    public boolean addNewStudent(String psu_id, String first_name, String last_name, String major, int credits) {
+    public static boolean addNewStudent(String psu_id, String first_name, String last_name, String major, int credits) {
         Student s = new Student(psu_id, first_name, last_name, major, credits);
         boolean add = alStudent.add(s);
         String message;
@@ -52,7 +42,7 @@ public class Storage {
         return true;
     }
 
-    public boolean addNewStudent(String psu_id, String first_name, String last_name, String major, String minor, int credits) {
+    public static boolean addNewStudent(String psu_id, String first_name, String last_name, String major, String minor, int credits) {
         Student s = new Student(psu_id, first_name, last_name, major, minor, credits);
         boolean add = alStudent.add(s);
         String message;
@@ -66,7 +56,7 @@ public class Storage {
         return true;
     }
 
-    public Student getStudent(int index) {
+    public static Student getStudent(int index) {
         if (index < 0 || index > alStudent.size()) {
             JOptionPane.showMessageDialog(null, "Index Out of Bounds", "Student", JOptionPane.ERROR_MESSAGE);
             return null;
@@ -74,7 +64,7 @@ public class Storage {
         return alStudent.get(index);
     }
 
-    public boolean addNewFaculty(String psu_id, String first_name, String last_name, String major_college, boolean[] preferred_days) {
+    public static boolean addNewFaculty(String psu_id, String first_name, String last_name, String major_college, boolean[] preferred_days) {
         Faculty f = new Faculty(psu_id, first_name, last_name, major_college, preferred_days);
         boolean add = alFaculty.add(f);
         String message;
@@ -88,7 +78,7 @@ public class Storage {
         return true;
     }
 
-    public Faculty getFaculty(int index) {
+    public static Faculty getFaculty(int index) {
         if (index < 0 || index > alFaculty.size()) {
             JOptionPane.showMessageDialog(null, "Index Out of Bounds", "Faculty", JOptionPane.ERROR_MESSAGE);
             return null;
@@ -96,7 +86,7 @@ public class Storage {
         return alFaculty.get(index);
     }
     
-    public boolean addNewRoom(String building, String room_id, int occupancy, int computers, String lab_type){
+    public static boolean addNewRoom(String building, String room_id, int occupancy, int computers, String lab_type){
         Room r = new Room(building, room_id, occupancy, computers, lab_type);
         boolean add = alRoom.add(r);
         String message;
@@ -110,7 +100,7 @@ public class Storage {
         return true;
     }
     
-    public Room getRoom(int index) {
+    public static Room getRoom(int index) {
         if (index < 0 || index > alRoom.size()) {
             JOptionPane.showMessageDialog(null, "Index Out of Bounds", "Room", JOptionPane.ERROR_MESSAGE);
             return null;
@@ -118,7 +108,7 @@ public class Storage {
         return alRoom.get(index);
     }
     
-    public boolean addNewCourse(String course_id, String subject, String course_num, char session, String name, String description, int units){
+    public static boolean addNewCourse(String course_id, String subject, String course_num, char session, String name, String description, int units){
         Course c = new Course(course_id, subject, course_num, session, name, description, units);
         boolean add = alCourse.add(c);
         String message;
@@ -132,7 +122,7 @@ public class Storage {
         return true;
     }
     
-    public boolean addNewCourse(String course_id, String subject, String course_num, char session, String name, String description, int units, int recommended_academic_planner_priority){
+    public static boolean addNewCourse(String course_id, String subject, String course_num, char session, String name, String description, int units, int recommended_academic_planner_priority){
         Course c = new Course(course_id, subject, course_num, session, name, description, units, recommended_academic_planner_priority);
         boolean add = alCourse.add(c);
         String message;
@@ -146,7 +136,7 @@ public class Storage {
         return true;
     }
     
-    public boolean addNewCourse(String course_id, String subject, String course_num, char session, String name, String description, int units, Course[] prereqs){
+    public static boolean addNewCourse(String course_id, String subject, String course_num, char session, String name, String description, int units, Course[] prereqs){
         Course c = new Course(course_id, subject, course_num, session, name, description, units, prereqs);
         boolean add = alCourse.add(c);
         String message;
@@ -160,7 +150,7 @@ public class Storage {
         return true;
     }
     
-    public boolean addNewCourse(String course_id, String subject, String course_num, char session, String name, String description, int units, int recommended_academic_planner_priority, Course[] prereqs){
+    public static boolean addNewCourse(String course_id, String subject, String course_num, char session, String name, String description, int units, int recommended_academic_planner_priority, Course[] prereqs){
         Course c = new Course(course_id, subject, course_num, session, name, description, units, recommended_academic_planner_priority, prereqs);
         boolean add = alCourse.add(c);
         String message;
@@ -174,7 +164,7 @@ public class Storage {
         return true;
     }
     
-    public Course getCourse(int index) {
+    public static Course getCourse(int index) {
         if (index < 0 || index > alCourse.size()) {
             JOptionPane.showMessageDialog(null, "Index Out of Bounds", "Course", JOptionPane.ERROR_MESSAGE);
             return null;
@@ -182,7 +172,7 @@ public class Storage {
         return alCourse.get(index);
     }
     
-    public boolean addNewTime_Period(LocalTime start_time, LocalTime end_time){
+    public static boolean addNewTime_Period(LocalTime start_time, LocalTime end_time){
         Time_Period t = new Time_Period(start_time, end_time);
         boolean add = alTime.add(t);
         String message;
@@ -196,7 +186,7 @@ public class Storage {
         return true;
     }
     
-    public Time_Period getTime_Period(int index) {
+    public static Time_Period getTime_Period(int index) {
         if (index < 0 || index > alTime.size()) {
             JOptionPane.showMessageDialog(null, "Index Out of Bounds", "Time", JOptionPane.ERROR_MESSAGE);
             return null;
@@ -204,7 +194,7 @@ public class Storage {
         return alTime.get(index);
     }
     
-    public boolean addNewSection(int courseIndex, String section, int capacity, boolean[] days){
+    public static boolean addNewSection(int courseIndex, String section, int capacity, boolean[] days){
         Section s = new Section(getCourse(courseIndex), section, capacity, days);
         boolean add = alSection.add(s);
         String message;
@@ -218,7 +208,7 @@ public class Storage {
         return true;
     }
     
-    public Section getSection(int index) {
+    public static Section getSection(int index) {
         if (index < 0 || index > alSection.size()) {
             JOptionPane.showMessageDialog(null, "Index Out of Bounds", "Section", JOptionPane.ERROR_MESSAGE);
             return null;
@@ -226,7 +216,7 @@ public class Storage {
         return alSection.get(index);
     }
     
-    public boolean addNewFCA(int facultyIndex, int timeIndex, int roomIndex, int sectionIndex){
+    public static boolean addNewFCA(int facultyIndex, int timeIndex, int roomIndex, int sectionIndex){
         Final_Course_Assignment fca = new Final_Course_Assignment(alFaculty.get(facultyIndex), alTime.get(timeIndex), alRoom.get(roomIndex), alSection.get(sectionIndex));
         boolean add = alFCA.add(fca);
         String message;
@@ -240,7 +230,7 @@ public class Storage {
         return true;
     }
     
-    public Final_Course_Assignment getFCA(int index) {
+    public static Final_Course_Assignment getFCA(int index) {
         if (index < 0 || index > alFCA.size()) {
             JOptionPane.showMessageDialog(null, "Index Out of Bounds", "Final Course Assignment", JOptionPane.ERROR_MESSAGE);
             return null;
