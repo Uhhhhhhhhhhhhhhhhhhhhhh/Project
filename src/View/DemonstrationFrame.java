93f57bcae0f231567c90c41f74e1e2ac6a5700b4
Derek <7885194+derekstephen@users.noreply.github.com>
2019-03-19 13:18:44 -0400

    Updates

--- src/View/DemonstrationFrame.java
+++ src/View/DemonstrationFrame.java
@@ -405,10 +405,6 @@ public class DemonstrationFrame extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
         
-        if(SQLStorage.SQLStorage(args[0], args[1], args[2])){
-            
-        }
-        
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
