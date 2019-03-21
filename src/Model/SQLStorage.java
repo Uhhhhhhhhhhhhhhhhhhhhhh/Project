6139cb78bdb07d6da7c62287c40e544536be85a4
FlimflamtheZimzam <lolercopter1.0@gmail.com>
2019-03-21 13:22:56 -0400

    SQL Storage
    Started working on the SQL storage code, added basic prepared statements and framework for insert statements, which need to be updated/finished.

--- src/Model/SQLStorage.java
+++ src/Model/SQLStorage.java
@@ -18,6 +18,48 @@ import javax.swing.JOptionPane;
 public class SQLStorage {
     
     private static Connection c;
+    private static PreparedStatement psInsFaculty;
+    private static PreparedStatement psInsRoom;
+    private static PreparedStatement psInsCourse;
+    private static PreparedStatement psInsPreReq;
+    private static PreparedStatement psInsSection;
+    private static PreparedStatement psInsTimePeriod;
+    private static PreparedStatement psInsProfTimePref;
+    private static PreparedStatement psInsFinalCourseAssignment;
+    private static PreparedStatement psGetFaculty;
+    private static PreparedStatement psGetRooms;
+    private static PreparedStatement psGetCourses;
+    private static PreparedStatement psGetPreReqs;
+    private static PreparedStatement psGetTimePeriod;
+    private static PreparedStatement psGetProfTimePrefs;
+    private static PreparedStatement psGetFinalCourseAssignments;
+    
+    public static void createPreparedStatements(Connection cnIn) throws SQLException {
+    String query = "INSERT into Faculty(PSU_ID, Last_Name, First_Name, Major, days)" + "values(?, ?, ?, ?, ?)";
+    psInsFaculty = c.prepareStatement(query);
+    
+    query = "INSERT into Room(building, room_id, occupancy, num_computers, lab_type)" + "values(?, ?, ?, ?, ?)";
+    psInsRoom = c.prepareStatement(query);
+    
+    query = "INSERT into Course(course_id, subject, course_num, description, units, rap_priority)" + "values(?, ?, ?, ?, ?, ?)";
+    psInsCourse = c.prepareStatement(query);
+    
+    query = "INSERT into Prereqs(course_id, prereq_id)" + "values(?, ?)";
+    psInsPreReq = c.prepareStatement(query);
+    
+    query = "INSERT into Sections(class_num, course_course_id)" + "values(?, ?)";
+    psInsSection = c.prepareStatement(query);
+    
+    query = "INSERT into Time_Period(building, room_id, occupancy, num_computers, lab_type)" + "values(?, ?, ?, ?, ?)";
+    psInsTimePeriod = c.prepareStatement(query);
+    
+    query = "INSERT into Professor_Time_Pref(building, room_id, occupancy, num_computers, lab_type)" + "values(?, ?, ?, ?, ?)";
+    psInsProfTimePref = c.prepareStatement(query);
+    
+    query = "INSERT into Final_Course_Assignment(building, room_id, occupancy, num_computers, lab_type)" + "values(?, ?, ?, ?, ?)";
+    psInsFinalCourseAssignment = c.prepareStatement(query);
+    
+    }
     
     public static boolean ConnectSQLStorage(String ip, String username, String password){
         try{  
@@ -48,17 +90,15 @@ public class SQLStorage {
     
     public static boolean addNewFaculty(String psu_id, String first_name, String last_name, String major_college, boolean[] preferred_days) {
         boolean success;
-        String query = "INSERT into Faculty(PSU_ID, Last_Name, First_Name, Major, days)" + "values(?, ?, ?, ?, ?)";
         
         try {
-            PreparedStatement preparedStmt = c.prepareStatement(query);
-            preparedStmt.setString (1, psu_id);
-            preparedStmt.setString (2, last_name);
-            preparedStmt.setString (3, first_name);
-            preparedStmt.setString (4, major_college);
-            preparedStmt.setInt    (5, Faculty.daysToInt(preferred_days));
+            psInsFaculty.setString (1, psu_id);
+            psInsFaculty.setString (2, last_name);
+            psInsFaculty.setString (3, first_name);
+            psInsFaculty.setString (4, major_college);
+            psInsFaculty.setInt    (5, Faculty.daysToInt(preferred_days));
 
-            success = preparedStmt.execute();
+            success = psInsFaculty.execute();
             JOptionPane.showMessageDialog(null, first_name + "'s information is saved.", "Faculty", JOptionPane.INFORMATION_MESSAGE);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "ERROR! FACULTY NOT CREATED!", "Faculty", JOptionPane.ERROR_MESSAGE);
@@ -67,25 +107,44 @@ public class SQLStorage {
         
         return success;
     }
-
-    public static Faculty getFacultyByPSUId(String psu_id) {
+    public static boolean addNewRoom(String building, String room_id, int occupancy, int num_computers, String lab_type) {
         boolean success;
-        String query = "SELECT * FROM Faculty WHERE PSU_ID = '" + psu_id + "'";
         
         try {
-            Statement st = c.createStatement();
+            psInsRoom.setString (1, building);
+            psInsRoom.setString (2, room_id);
+            psInsRoom.setInt (3, occupancy);
+            psInsRoom.setInt (4, num_computers);
+            psInsRoom.setString (5, lab_type);
             
-            ResultSet rs = st.executeQuery(query);
-            
-            return new Faculty(rs.getString("PSU_ID"), rs.getString("first_name"), rs.getString("last_name"), rs.getString("major"), Faculty.intToArray(rs.getInt("days")));
+            success = psInsRoom.execute();
+            JOptionPane.showMessageDialog(null, building + room_id + " information is saved.", "Room", JOptionPane.INFORMATION_MESSAGE);
         } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "ERROR! COULD NOT GRAB INFORMATION!", "Faculty", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "ERROR! ROOM NOT CREATED!", "Room", JOptionPane.ERROR_MESSAGE);
             success = false;
-            return null;
         }
         
+        return success;
     }
-    
+    public static boolean addNewCourse(String course_id, String subject, String course_num, String description, int units, int rap_priority) {
+        boolean success;
+        
+        try {
+            psInsCourse.setString (1, course_id);
+            psInsCourse.setString (2, subject);
+            psInsCourse.setString (3, course_num);
+            psInsCourse.setString (4, description);
+            psInsCourse.setInt (5, units);
+            psInsCourse.setInt (6, rap_priority);
+            
+            success = psInsCourse.execute();
+            JOptionPane.showMessageDialog(null, subject + course_id + " information is saved.", "Room", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "ERROR! COURSE NOT CREATED!", "Course", JOptionPane.ERROR_MESSAGE);
+            success = false;
+        }
+        return success;
+    }
     
     
     
