0701e0f8b392d343d7836f990561b04d64348527
mzw171 <42522351+mzw171@users.noreply.github.com>
2019-04-24 22:24:44 -0400

    Haahahahaeee
    Added some faculty, room, and course data

--- IST261DBTables.sql
+++ IST261DBTables.sql
@@ -101,3 +101,47 @@ values ('0101000', '08:00:00', '08:55:00'),
 ('0101000', '16:35:00', '17:50:00'),
 ('0101000', '18:00:00', '19:15:00'),
 ('0101000', '19:30:00', '20:45:00');
+
+INSERT into Faculty(PSU_ID, Last_Name, First_Name, Major_College, Employment_Type)
+values ('qry323', 'Snaps', 'Ginger', 'IST/SRA', 'Tenure'),
+('crm13', 'Maurer', 'Clifford', 'IST/SRA', 'Full-Time'),
+('auk3', 'Konak', 'Abdullah', 'IST/SRA', 'Full-Time'),
+('tkc3', 'Clark', 'Tricia', 'IST/SRA', 'Tenure'),
+('yxx78', 'Xue', 'Yuan', 'IST/SRA', 'Tenure'),
+('dzl5300', 'Liu', 'Daonian', 'IST/SRA', 'Tenure'),
+('mxn16', 'Nasereddin', 'Mahdi', 'IST/SRA', 'Full-Time'),
+('whb108', 'Bowers', 'William', 'IST/SRA', 'Part-Time'),
+('mrb24', 'Bartolacci', 'Michael', 'IST/SRA', 'Part-Time'),
+('sxk70', 'Kulturel-Konak', 'Sadan', 'IST/SRA', 'Tenure'),
+('jkb20', 'Burk', 'Jill', 'CAS', 'Full-Time'),
+('cln12', 'Cheryl', 'Nicholas', 'CAS', 'Full-Time'),
+('kmm410', 'Williams', 'Kesha', 'CAS', 'Part-Time'),
+('cdc18', 'Catanach', 'Catherine', 'CAS', 'Part-Time'),
+('msr332', 'Ramsay', 'Michele', 'CAS', 'Tenure'),
+('dpr15', 'Reitz', 'Dawn', 'CAS', 'Tenure'),
+('amir.barakati', 'Barakati', 'Amir', 'Engineering', 'Full-Time'),
+('aue10', 'Panah', 'Azar', 'Engineering', 'Tenure'),
+('kms122', 'Hauser', 'Kathleen', 'Engineering', 'Full-Time');
+
+INSERT into Room(Room_ID, Building, Occupancy, Num_of_Computers, Lab_Type)
+values ('208', 'Gaige', 30, 30, 'Computer'),
+('207', 'Gaige', 25, 20, 'Computer'),
+('210', 'Gaige', 25, 20, 'Computer'),
+('209', 'Gaige', 20, 20, 'Computer'),
+('218', 'Gaige', 45, 0, 'Lecture'),
+('110', 'Gaige', 30, 1, 'Lecture'),
+('120', 'Luerrson', 20, 20, 'Computer'),
+('013', 'Luerrson', 40, 1, 'Lecture'),
+('210', 'Luerrson', 15, 2, 'Fluid');
+
+INSERT into Course(Sub, Course_Num, course_name, Description, Units)
+values ('IST', '110','Information, People and Technology', 'Test Description for IST 110', 3),
+('SRA', '111','Introduction to Security and Risk Analysis', 'Test Description for SRA 111', 3),
+('IST', '210','Database Software', 'Test Description for IST 210', 3),
+('IST', '220','Networking Technology', 'Test Description for IST 220', 3),
+('SRA', '211','Threat of Terrorism and Crime', 'Test Description for SRA 211', 3),
+('IST', '140','Intro to Python', 'Test Description for IST 140', 3),
+('IST', '242','Intermediate Object Oriented Programming', 'Test Description for IST 242', 3),
+('CAS', '100A','Intro to Communication Arts and Sciences', 'Test Description for CAS 100A', 3),
+('CAS', '100B','Intro to Communication Arts and Sciences', 'Test Description for CAS 100B', 3),
+('CAS', '100C','Intro to Communication Arts and Sciences', 'Test Description for CAS 100C', 3);
